ad6f35b15f1d8853df213ca6a54d02bbeaa29afd
essk13 <kldoggod@gmail.com>
2022-01-05 01:51:49 +0900

    add algorithm 0104

--- algorithm/01_problem/2022/01/0104/9376_BAEKJOON.py
+++ algorithm/01_problem/2022/01/0104/9376_BAEKJOON.py
@@ -1,79 +1,66 @@
-import sys, heapq
-from copy import deepcopy
+import sys
+from collections import deque
 input = sys.stdin.readline
+sys.setrecursionlimit(10000)
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
-'''
-FAIL
-'''
 
-def escape1(r, c):
-    root = []
-    heapq.heappush(root, (0, r, c, []))
-    visited = [[0] * w for _ in range(h)]
-    cnt = 21e8
-    while root:
-        door_cnt, y, x, door_position = heapq.heappop(root)
-        if y in (0, h - 1) or x in (0, w - 1):
-            res = door_cnt + escape2(door_position, prisoner[1])
-            cnt = min(cnt, res)
-
+def escape(position):
+    que = deque([position])
+    # 음수는 지나지 않은 길
+    visited = [[-1] * (w + 2) for _ in range(h + 2)]
+    # 시작점 0으로 초기화
+    visited[position[0]][position[1]] += 1
+    while que:
+        r, c = que.pop()
         for i in range(4):
-            nr, nc = y + dr[i], x + dc[i]
-            if nr < 0 or nr >= h or nc < 0 or nc >= w: continue
-            if visited[nr][nc]: continue
+            nr, nc = r + dr[i], c + dc[i]
+            # 범위를 벗어나거나 방문한 지점, 벽은 무시
+            if nr < 0 or nr >= h + 2 or nc < 0 or nc >= w + 2: continue
+            if visited[nr][nc] >= 0: continue
             if MAP[nr][nc] == '*': continue
 
-            visited[nr][nc] = 1
+            # 문을 통과하는 경우 카운트 +1
             if MAP[nr][nc] == '#':
-                heapq.heappush(root, (door_cnt + 1, nr, nc, door_position + [(nr, nc)]))
+                visited[nr][nc] = visited[r][c] + 1
+                que.appendleft((nr, nc))
+            # 통로를 지나는 경우 이전 카운트 유지
             else:
-                heapq.heappush(root, (door_cnt, nr, nc, door_position))
-    return cnt
-
-
-def escape2(position, prisoner_position):
-    new_map = deepcopy(MAP)
-    for y, x in position:
-        new_map[y][x] = '.'
-
-    r, c = prisoner_position
-    min_ = []
-    heapq.heappush(min_, (0, r, c))
-    visited = [[0] * w for _ in range(h)]
-    while min_:
-        door_cnt, y, x = heapq.heappop(min_)
-        if y in (0, h - 1) or x in (0, w - 1):
-            return door_cnt
-
-        for i in range(4):
-            nr, nc = y + dr[i], x + dc[i]
-            if nr < 0 or nr >= h or nc < 0 or nc >= w: continue
-            if visited[nr][nc]: continue
-            if new_map[nr][nc] == '*': continue
-
-            visited[nr][nc] = 1
-            if new_map[nr][nc] == '#':
-                heapq.heappush(min_, (door_cnt + 1, nr, nc))
-            else:
-                heapq.heappush(min_, (door_cnt, nr, nc))
-    return 0
+                visited[nr][nc] = visited[r][c]
+                que.append((nr, nc))
+    return visited
 
 
 for tc in range(int(input())):
     h, w = map(int, input().split())
-    MAP = [list(input().strip()) for _ in range(h)]
-
-    ans = 0
+    # 벽 옆으로 빈 공간 생성
+    MAP = [['.' for _ in range(w + 2)]] + [['.'] + list(input().strip()) + ['.'] for _ in range(h)] + [['.' for _ in range(w + 2)]]
     prisoner = []
-    for r in range(h):
-        for c in range(w):
+    for r in range(h + 2):
+        for c in range(w + 2):
             if MAP[r][c] == '$':
                 prisoner.append((r, c))
             if len(prisoner) == 2:
                 break
 
-    r, c = prisoner[0]
-    ans = escape1(r, c)
+    # 탈옥수와 상근이의 이동루트 별 개방한 문의 개수 기록
+    prisoner1 = escape(prisoner[0])
+    prisoner2 = escape(prisoner[1])
+    sang_geun = escape((0, 0))
+
+    ans = 100 ** 2
+    for r in range(h + 2):
+        for c in range(w + 2):
+            '''
+            1) 3명이 모두 만나는 지점 확인
+            2) 각각의 개방한 문 누적
+            3) 해당 지점이 문인 경우 중복된 2개의 문 제거
+            4) 이전 값과 현재 값 비교
+            '''
+            if prisoner1[r][c] >= 0 and prisoner2[r][c] >= 0 and sang_geun[r][c] >= 0:
+                res = prisoner1[r][c] + prisoner2[r][c] + sang_geun[r][c]
+                if MAP[r][c] == '#':
+                    res -= 2
+                ans = min(ans, res)
     print(ans)
